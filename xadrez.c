3996b15bc2571d86e9768331ebb84af1bf957a5b
Anna Clara <annajanampc@gmail.com>
2025-06-24 20:14:26 +0000

    Nivel Aventureiro

--- xadrez.c
+++ xadrez.c
@@ -3,35 +3,50 @@
 int main(){
 
     //MOVIMENTAÇÃO TORRE (CINCO CASAS PARA A DIREITA)
-        int t = 0;
+        int i = 0;
      
              printf("***MOVIMENTAÇÃO TORRE*** \n");
-    while (t< 5) {
-        printf("Direita %d \n", t);
-        t++;
+    while (i< 5) {
+        printf("Direita %d \n", i);
+        i++;
     }
 
     
     //MOVIMENTAÇÃO BISPO (CINCO CASAS NA DIAGONAL CIMA/DIREITA)
  
-    int b = 0;
+    int j = 0;
                printf("***MOVIMENTAÇÃO BISPO*** \n");
     do
     {
-        printf("Cima Direita %d \n", b);
-        b++;
-    } while (b < 5);
+        printf("Cima Direita %d \n", j);
+        j++;
+    } while (j < 5);
     
 
     //MOVIMENTAÇÃO RAINHA (OITO CASAS A ESQUERDA)
 
                 printf("***MOVIMENTAÇÃO RAINHA** \n");
 
-    for (int r = 0; r < 8; r++)
+    for (int k = 0; k < 8; k++)
     {
-       printf("Esquerda %d \n", r);
+       printf("Esquerda %d \n", k);
     }
     
+
+    //MOVIMENTAÇÃO CAVALO (DUAS CASAS PARA BAIXO E UMA PARA ESQUERDA)
+  
+             printf("***MOVIMENTAÇÃO CAVALO** \n");
+    int m = 0;
+    
+   for (int l=0; l < 1; l++){
+    while (m < 2){
+    
+        printf("Baixo %d \n", m);
+        m++;
+    }
+    printf("Esquerda %d \n", l);
+
+   }
 
     return 0;
 }
